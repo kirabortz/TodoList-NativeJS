1267d3770ed238551337dd4b61b9b0695006e716
BladeDancer <69kir969@mail.ru>
2023-07-24 21:52:52 +0300

    commit

--- index.js
+++ index.js
@@ -1,114 +1,146 @@
-let storage = [];
-let allTasksCompletedState = false;
-
-let newTaskInput = document.querySelector(".new_task_input");
-let tasksList = document.querySelector(".tasks_list");
-let statusBtns = document.querySelector(".status_btns");
-let countTask = document.querySelector("#count_task");
-let completeAllTasksBtn = document.querySelector(".complete_all_tasks_btn");
-let allBtn = document.querySelector(".all_btn");
-let activeBtn = document.querySelector(".active_btn");
-let completedBtn = document.querySelector(".completed_btn");
-let clearCompletedBtn = document.querySelector(".clear_completed_btn");
-
-newTaskInput.addEventListener("keyup", (e) => {
-  if (!newTaskInput.value) return;
-  if (e.keyCode === 13) AddTask(newTaskInput.value), (newTaskInput.value = "");
-});
-completeAllTasksBtn.addEventListener("click", () => AllTaskDone());
-allBtn.addEventListener("click", () => RerenderTasks(filterAllTasks()));
-activeBtn.addEventListener("click", () => RerenderTasks(filterActiveTasks()));
-completedBtn.addEventListener("click", () => RerenderTasks(filterCompletedTasks()));
-clearCompletedBtn.addEventListener("click", () => DeleteAllTaskDone());
-
-let filterAllTasks = () => storage;
-let filterActiveTasks = () => storage.filter((task) => !task.completed);
-let filterCompletedTasks = () => storage.filter((task) => task.completed);
-
-const RerenderTasks = (filteredTasks) => {
-  tasksList.innerHTML = "";
-  if (filteredTasks.length === 0) return;
-
-  filteredTasks.forEach((task) => {
-    const taskItem = document.createElement("div");
-    const taskInput = document.createElement("input");
-    const completeTaskBtn = document.createElement("button");
-    completeTaskBtn.classList.add("complete_task_btn");
-    const deleteTaskBtn = document.createElement("button");
-
-    deleteTaskBtn.classList.add("delete_task_btn");
-    taskInput.classList.add("task_input");
-    taskItem.classList.add("task_block");
-    console.log(taskItem);
-    taskInput.value = task.value;
-    taskItem.id = task.id;
-    taskItem.append(completeTaskBtn, taskInput, deleteTaskBtn);
-
-    if (task.completed) {
-      taskInput.classList.toggle("done");
-      completeTaskBtn.classList.toggle("complete_task_btn_done");
-    }
-    if (!document.getElementById(task.id)) {
-      deleteTaskBtn.addEventListener("click", () => DelTask(taskItem.id));
-      completeTaskBtn.addEventListener("click", () => TaskDone(taskItem.id));
-      tasksList.append(taskItem);
-    }
-  });
-};
-
-const AddTask = (inputValue) => {
-  const uniqueId = Math.random().toString(36).substr(2, 9);
-  storage.push({ id: uniqueId, value: inputValue, completed: false });
-  ChangeCountTasks();
-  RerenderTasks(filterAllTasks());
-  console.log(storage);
-};
-
-const DelTask = (id) => {
-  const index = storage.findIndex((obj) => obj.id === id);
-  if (index !== -1) {
-    storage.splice(index, 1);
-    const taskItem = document.getElementById(id);
-    taskItem && taskItem.remove();
+class TodoList {
+  constructor() {
+    this.tasks = [];
+    this.completeAllTasksBtn = document.querySelector(".complete_all_tasks_btn");
+    this.taskList = document.querySelector(".task_list");
+    this.newTaskInput = document.querySelector(".new_task_input");
+    this.statusBtns = document.querySelector(".status_btns");
+    this.countTask = document.querySelector(".count_task");
+    this.tabs = document.querySelectorAll(".tab");
+    this.activeTab = "all";
+    this.clearCompletedBtn = document.querySelector(".clear_completed_btn");
+    this.render();
   }
-  ChangeCountTasks();
-};
 
-const TaskDone = (id) => {
-  const task = storage.find((task) => task.id === id);
-  task.completed = !task.completed;
-  RerenderTasks(filterAllTasks());
-  ChangeCountTasks();
-};
+  renderTasks() {
+    this.completeAllTasksBtn.classList.toggle("hidden", this.tasks.length === 0);
+    this.statusBtns.classList.toggle("hidden", this.tasks.length === 0);
+    if (this.tasks.some((task) => task.completed)) {
+      this.clearCompletedBtn.classList.remove("visible");
+    } else {
+      this.clearCompletedBtn.classList.add("visible");
+    }
 
-const AllTaskDone = () => {
-  storage.map((task) => {
-    task.completed = !allTasksCompletedState ? true : false;
-  });
-  completeAllTasksBtn.classList.toggle("active_all_tasks_btn");
-  allTasksCompletedState = !allTasksCompletedState;
-  RerenderTasks(filterAllTasks());
-  ChangeCountTasks();
-};
+    this.taskList.innerHTML = "";
+    const filteredTasks = this.tasks.filter((task) => {
+      if (this.activeTab === "all") {
+        return true;
+      } else if (this.activeTab === "active") {
+        return !task.completed;
+      } else if (this.activeTab === "completed") {
+        return task.completed;
+      }
+    });
+    filteredTasks.forEach((task) => {
+      const taskBlock = document.createElement("div");
+      taskBlock.classList.add("task_block");
+      const taskDoneBtn = document.createElement("button");
+      taskDoneBtn.classList.add("complete_task_btn");
+      taskDoneBtn.addEventListener("click", () => {
+        task.completed = !task.completed;
+        this.renderTasks();
+      });
 
-const DeleteAllTaskDone = () => {
-  storage = storage.filter((task) => !task.completed);
-  RerenderTasks(filterAllTasks());
-  ChangeCountTasks();
-};
+      const taskInput = document.createElement("input");
+      taskInput.classList.add("task_input");
+      taskInput.value = task.value;
+      taskInput.addEventListener("dblclick", () => {
+        taskDoneBtn.style.visibility = "hidden";
+        taskDeleteBtn.style.display = "none";
+        const editTaskInput = document.createElement("input");
+        editTaskInput.classList.add("edit_input");
+        editTaskInput.style.border = "1px solid black";
+        editTaskInput.value = task.value;
+        taskInput.replaceWith(editTaskInput);
+        editTaskInput.focus();
+        editTaskInput.addEventListener("blur", () => {
+          taskDoneBtn.style.visibility = "visible";
+          taskDeleteBtn.style.display = "block";
+          editTaskInput.style.border = "none";
+          editTaskInput.replaceWith(taskInput);
+          task.value = editTaskInput.value;
+          taskInput.value = task.value;
 
-const ChangeCountTasks = () => {
-  countTask.innerText = `${filterActiveTasks().length} items left`;
-  BlocksVisibility();
-};
+          if (taskInput.value.length === 0) {
+            this.tasks = this.tasks.filter((item) => item.id !== task.id);
+            taskBlock.remove();
+            this.renderTasks();
+          }
+          localStorage.setItem("tasks", JSON.stringify(this.tasks));
+        });
+      });
 
-const BlocksVisibility = () => {
-  statusBtns.style.display = completeAllTasksBtn.style.display = storage.length === 0 ? "none" : "flex";
-  completeAllTasksBtn.classList.toggle("complete_all_tasks_btn", storage.length !== 0);
-  clearCompletedBtn.style.visibility = filterCompletedTasks().length === 0 ? "hidden" : "visible";
-};
+      const taskDeleteBtn = document.createElement("button");
+      taskDeleteBtn.classList.add("delete_task_btn");
+      taskDeleteBtn.addEventListener("click", () => {
+        this.tasks = this.tasks.filter((t) => t.id !== task.id);
+        localStorage.setItem("tasks", JSON.stringify(this.tasks));
+        this.renderTasks();
+      });
+      if (task.completed) {
+        taskInput.classList.toggle("done");
+        taskDoneBtn.classList.toggle("complete_task_btn");
+        taskDoneBtn.classList.toggle("complete_task_btn_done");
+        localStorage.setItem("tasks", JSON.stringify(this.tasks));
+      }
+      taskBlock.appendChild(taskDoneBtn);
+      taskBlock.appendChild(taskInput);
+      taskBlock.appendChild(taskDeleteBtn);
+      this.taskList.appendChild(taskBlock);
+    });
+    this.countTask.innerText = `${this.tasks.filter((task) => !task.completed).length} items left`;
+  }
+  allTasksDone() {
+    let allTasksCompletedState = false;
+    this.completeAllTasksBtn.addEventListener("click", () => {
+      this.tasks.forEach((task) => (task.completed = !allTasksCompletedState ? true : false));
+      allTasksCompletedState = !allTasksCompletedState;
+      this.completeAllTasksBtn.classList.toggle("active_all_tasks_btn");
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
+      this.renderTasks();
+    });
+  }
+  selectTab() {
+    this.tabs.forEach((tab) => {
+      tab.addEventListener("click", () => {
+        this.activeTab = tab.dataset.tab;
+        this.renderTasks();
+      });
+    });
+  }
+  addNewTask() {
+    this.newTaskInput.addEventListener("keyup", (e) => {
+      if (e.keyCode === 13) {
+        if (this.newTaskInput.value.trim()) {
+          const uniqueId = Math.random().toString(36).substr(2, 9);
+          const task = {
+            id: uniqueId,
+            value: this.newTaskInput.value.trim(),
+            completed: false,
+          };
+          this.tasks.push(task);
+          localStorage.setItem("tasks", JSON.stringify(this.tasks));
+          this.renderTasks();
+          this.newTaskInput.value = "";
+        }
+      }
+    });
+  }
+  deleteAllCompletedTasks() {
+    this.clearCompletedBtn.addEventListener("click", () => {
+      this.tasks = this.tasks.filter((task) => !task.completed);
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
+      this.renderTasks();
+    });
+  }
+  render() {
+    this.tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    this.renderTasks();
+    this.allTasksDone();
+    this.selectTab();
+    this.addNewTask();
+    this.deleteAllCompletedTasks();
+  }
+}
 
-window.addEventListener("load", () => {
-  RerenderTasks(filterAllTasks());
-  ChangeCountTasks();
-});
+const todoList = new TodoList();
