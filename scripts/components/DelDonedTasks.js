8604e1a0cbef65b97bba27d098a521a783f383b4
BladeDancer <69kir969@mail.ru>
2023-11-14 14:54:55 +0300

    create DelDonedTasks func

--- scripts/components/DelDonedTasks.js
+++ scripts/components/DelDonedTasks.js
@@ -1,10 +1,13 @@
 import { RenderTasks } from "./RenderTasks.js";
 import { StorageSet } from "./Storage.js";
 
-export function DelDonedTasks(tasks, clearCompletedBtn) {
+export function DelDonedTasks(tasks, activeTab) {
+  let clearCompletedBtn = document.querySelector(".clear_completed_btn");
+
   clearCompletedBtn.addEventListener("click", () => {
     tasks = tasks.filter((task) => !task.completed);
+
     StorageSet(tasks);
-    RenderTasks();
+    RenderTasks(tasks, activeTab);
   });
 }
