4913994567b1d531c553baa5b27d15f676f33af1
BladeDancer <69kir969@mail.ru>
2023-11-14 14:37:23 +0300

    create Visibility func

--- scripts/components/Visibility.js
+++ scripts/components/Visibility.js
@@ -1,4 +1,8 @@
-export function Visibility(completeAllTasksBtn, statusBtns, tasks, clearCompletedBtn) {
+export function Visibility(tasks) {
+  let statusBtns = document.querySelector(".status_btns");
+  let clearCompletedBtn = document.querySelector(".clear_completed_btn");
+  let completeAllTasksBtn = document.querySelector(".complete_all_tasks_btn");
+
   completeAllTasksBtn.classList.toggle("hidden", tasks.length === 0);
   statusBtns.classList.toggle("hidden", tasks.length === 0);
 
