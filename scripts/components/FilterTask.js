fba7884f6de897d809c97bbc92b02a17b789c746
BladeDancer <69kir969@mail.ru>
2023-11-14 14:39:49 +0300

    create FilterTask func

--- scripts/components/FilterTask.js
+++ scripts/components/FilterTask.js
@@ -1,17 +1,4 @@
-import { RenderTasks } from "./RenderTasks.js";
-
-let activeTab = "all";
-let tabs = document.querySelectorAll(".tab");
-tabs.forEach((tab) => {
-  tab.classList.remove("active");
-  tab.addEventListener("click", () => {
-    activeTab = tab.dataset.tab;
-    tab.classList.add("active");
-    RenderTasks();
-  });
-});
-
-export function FilterTask(tasks) {
+export function FilterTask(tasks, activeTab) {
   return tasks.filter((task) => {
     if (activeTab === "all") {
       return true;
