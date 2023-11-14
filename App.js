1f3e401acd1cb85722a856fa771e17d1c0bb3684
BladeDancer <69kir969@mail.ru>
2023-11-14 14:58:44 +0300

    create func launch the project (App)

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
+import { StorageParse } from "./scripts/components/Storage.js";
+import { AllTasksDone } from "./scripts/components/AllTasksDone.js";
+import { DelDonedTasks } from "./scripts/components/DelDonedTasks.js";
+import { TabState } from "./scripts/components/TabState.js";
 import { RenderTasks } from "./scripts/components/RenderTasks.js";
 
 export function App() {
-  RenderTasks();
+  let tasks = StorageParse();
+  let activeTab = "all";
+
+  AllTasksDone(tasks, TabState(activeTab));
+
+  DelDonedTasks(tasks, TabState(activeTab));
+
+  RenderTasks(tasks, TabState(activeTab));
 }
 App();
