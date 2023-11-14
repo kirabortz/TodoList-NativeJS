67136ce5bf48c0b056b25b6ad560845a0b45485f
BladeDancer <69kir969@mail.ru>
2023-11-14 14:55:40 +0300

    create AllTasksDone func

--- scripts/components/AllTasksDone.js
+++ scripts/components/AllTasksDone.js
@@ -1,14 +1,17 @@
 import { RenderTasks } from "./RenderTasks.js";
 import { StorageSet } from "./Storage.js";
 
-export function AllTasksDone(completeAllTasksBtn, tasks) {
+export function AllTasksDone(tasks, activeTab) {
+  let completeAllTasksBtn = document.querySelector(".complete_all_tasks_btn");
+
   let allTasksCompletedState = false;
   completeAllTasksBtn.addEventListener("click", () => {
     tasks.forEach((task) => (task.completed = !allTasksCompletedState));
 
     allTasksCompletedState = !allTasksCompletedState;
     completeAllTasksBtn.classList.toggle("active_all_tasks_btn");
+
     StorageSet(tasks);
-    RenderTasks();
+    RenderTasks(tasks, activeTab);
   });
 }
