b2ecaa842b34a30f4d578bd7c890c3789940b7b0
BladeDancer <69kir969@mail.ru>
2023-11-15 14:24:54 +0300

    added a variable (tasks)

--- scripts/components/DelDonedTasks.js
+++ scripts/components/DelDonedTasks.js
@@ -1,13 +1,14 @@
 import { RenderTasks } from "./RenderTasks.js";
-import { StorageSet } from "./Storage.js";
+import { StorageParse, StorageSet } from "./Storage.js";
 
-export function DelDonedTasks(tasks, activeTab) {
+export function DelDonedTasks(activeTab) {
   let clearCompletedBtn = document.querySelector(".clear_completed_btn");
 
   clearCompletedBtn.addEventListener("click", () => {
+    let tasks = StorageParse();
     tasks = tasks.filter((task) => !task.completed);
 
     StorageSet(tasks);
-    RenderTasks(tasks, activeTab);
+    RenderTasks(activeTab);
   });
 }
