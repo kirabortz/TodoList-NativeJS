288ec2ef774f2e2f8d7a06b87ed2bcce4bd19e25
BladeDancer <69kir969@mail.ru>
2023-11-15 14:26:25 +0300

    removed the argument (tasks), added a variable (tasks)

--- scripts/components/RenderTasks.js
+++ scripts/components/RenderTasks.js
@@ -3,18 +3,17 @@ import { AddNewTask } from "./AddNewTask.js";
 import { FilterTask } from "./FilterTask.js";
 import { TaskBlock } from "./TaskBlock.js";
 import { Visibility } from "./Visibility.js";
-import { SelectTab } from "./SelectTab.js";
+import { StorageParse } from "./Storage.js";
 
-export function RenderTasks(tasks, activeTab) {
-  SelectTab(tasks);
+export function RenderTasks(activeTab) {
+  let tasks = StorageParse();
+  AddNewTask(activeTab);
 
   let filteredTasks = FilterTask(tasks, activeTab);
 
   Visibility(tasks);
 
-  AddNewTask(tasks, activeTab);
-
-  TaskBlock(tasks, filteredTasks, activeTab);
+  TaskBlock(filteredTasks, activeTab);
 
   ActiveCounter(tasks);
 }
