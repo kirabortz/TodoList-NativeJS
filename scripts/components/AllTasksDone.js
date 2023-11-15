bf1c8573a4c2c726241695915feb595ac08ba50e
BladeDancer <69kir969@mail.ru>
2023-11-15 14:18:57 +0300

    added a new variable (tasks), git commit -m

--- scripts/components/AllTasksDone.js
+++ scripts/components/AllTasksDone.js
@@ -1,17 +1,17 @@
 import { RenderTasks } from "./RenderTasks.js";
-import { StorageSet } from "./Storage.js";
+import { StorageParse, StorageSet } from "./Storage.js";
 
-export function AllTasksDone(tasks, activeTab) {
+export function AllTasksDone(activeTab) {
   let completeAllTasksBtn = document.querySelector(".complete_all_tasks_btn");
 
   let allTasksCompletedState = false;
   completeAllTasksBtn.addEventListener("click", () => {
+    let tasks = StorageParse();
     tasks.forEach((task) => (task.completed = !allTasksCompletedState));
-
+    StorageSet(tasks);
     allTasksCompletedState = !allTasksCompletedState;
     completeAllTasksBtn.classList.toggle("active_all_tasks_btn");
 
-    StorageSet(tasks);
-    RenderTasks(tasks, activeTab);
+    RenderTasks(activeTab);
   });
 }
