7dd5aa4d6032b378d9b3b455b1fcc867b8a26f2c
BladeDancer <69kir969@mail.ru>
2023-11-15 14:24:06 +0300

    removed argument (tasks)

--- scripts/components/CompleteTask.js
+++ scripts/components/CompleteTask.js
@@ -1,19 +1,15 @@
 import { RenderTasks } from "./RenderTasks.js";
 import { StorageSet } from "./Storage.js";
 
-export function CompleteTask(tasks, task, taskDoneBtn, filteredTasks, taskLabel, activeTab) {
+export function CompleteTask(task, taskDoneBtn, filteredTasks, taskLabel, activeTab) {
+  taskDoneBtn.addEventListener("click", () => {
+    task.completed = !task.completed;
+    StorageSet(filteredTasks);
+    RenderTasks(activeTab);
+  });
   if (task.completed) {
     taskLabel.classList.toggle("done");
     taskDoneBtn.classList.toggle("complete_task_btn");
     taskDoneBtn.classList.toggle("complete_task_btn_done");
-
-    StorageSet(filteredTasks);
   }
-  taskDoneBtn.addEventListener("click", () => {
-    task.completed = !task.completed;
-
-    StorageSet(tasks);
-
-    RenderTasks(tasks, activeTab);
-  });
 }
