52262375c878bb8679df6be039f8341a9b4afbff
BladeDancer <69kir969@mail.ru>
2023-11-15 14:26:51 +0300

    removed the argument (tasks)

--- scripts/components/SelectTab.js
+++ scripts/components/SelectTab.js
@@ -1,14 +1,14 @@
 import { RenderTasks } from "./RenderTasks.js";
 import { TabState } from "./TabState.js";
 
-export function SelectTab(tasks) {
+export function SelectTab() {
   let tabs = document.querySelectorAll(".tab");
   tabs.forEach((tab) => {
     tab.classList.remove("active");
     tab.addEventListener("click", () => {
       tab.classList.add("active");
 
-      RenderTasks(tasks, TabState(tab.dataset.tab));
+      RenderTasks(TabState(tab.dataset.tab));
     });
   });
 }
