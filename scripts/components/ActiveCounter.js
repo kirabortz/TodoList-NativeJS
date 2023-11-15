28b6e7611e0dfebc77a73a5b173f74c08bc83637
BladeDancer <69kir969@mail.ru>
2023-11-15 14:11:52 +0300

    delete comment slashs

--- scripts/components/ActiveCounter.js
+++ scripts/components/ActiveCounter.js
@@ -2,4 +2,3 @@ export function ActiveCounter(tasks) {
   let countTask = document.querySelector(".count_task");
   countTask.innerText = `${tasks.filter((task) => !task.completed).length} items left`;
 }
-//
