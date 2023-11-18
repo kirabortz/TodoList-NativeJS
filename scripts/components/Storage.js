11189d483de9bd1bb67ccc3c7fa13e1fb706a915
BladeDancer <69kir969@mail.ru>
2023-11-18 15:50:09 +0300

    changed the function to an arrow and add 2 new func for tabs in LocalStorage

--- scripts/components/Storage.js
+++ scripts/components/Storage.js
@@ -1,7 +1,11 @@
-export function StorageParse() {
-  return JSON.parse(localStorage.getItem("tasks")) || [];
-}
+export const StorageParse = () => JSON.parse(localStorage.getItem("tasks")) || [];
 
-export function StorageSet(tasks) {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+export const StorageSet = (tasks) => localStorage.setItem("tasks", JSON.stringify(tasks));
+
+export const StorageTabParse = () => {
+  const selectedTab = JSON.parse(localStorage.getItem("selectedTab")) || "all";
+  return document.querySelector(`[data-tab="${selectedTab}"]`);
+};
+export const StorageTabSet = (selectedTab) => {
+  localStorage.setItem("selectedTab", JSON.stringify(selectedTab.dataset.tab));
+};
