6c46e8ed1163ebf0f438c911623e7db8955841b4
BladeDancer <69kir969@mail.ru>
2023-11-22 11:37:28 +0300

    renamed component

---
+++ scripts/components/SetActiveTab.js
@@ -0,0 +1,35 @@
+import { updateTaskList } from "./updateTaskList.js";
+import { getLocalStorageTabParse, setLocalStorage } from "./LocalStorage.js";
+
+export const setActiveTab = (activeTab) => {
+  const tabs = document.querySelectorAll(".tab");
+
+  tabs.forEach((tab) => {
+    if (activeTab) {
+      tab.classList.remove("active");
+      tab.classList.remove("selected");
+
+      activeTab.classList.add("active");
+      activeTab.classList.add("selected");
+    }
+    tab.addEventListener("click", () => {
+      setCurrentTab(tab);
+    });
+  });
+
+  const setCurrentTab = (selectedTab) => {
+    setLocalStorage(selectedTab.dataset.tab);
+
+    const activeTab = getLocalStorageTabParse();
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+      tab.classList.remove("selected");
+    });
+
+    activeTab.classList.add("active");
+    activeTab.classList.add("selected");
+
+    updateTaskList(activeTab);
+  };
+};
