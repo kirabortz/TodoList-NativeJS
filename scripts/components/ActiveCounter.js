1d969e2de8028c6c3922bc8b3a8de0840e693e8a
BladeDancer <69kir969@mail.ru>
2023-11-22 11:57:13 +0300

    renamed

--- scripts/components/ActiveCounter.js
+++
@@ -1,4 +0,0 @@
-export const ActiveCounter = (tasks) => {
-  const countTask = document.querySelector(".count_task");
-  countTask.innerText = `${tasks.filter((task) => !task.completed).length} items left`;
-};
