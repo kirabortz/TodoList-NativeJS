0b7d8822ead292a870043c68d105bb6d0024dbd7
BladeDancer <69kir969@mail.ru>
2023-11-22 16:56:51 +0300

    fixed import func name

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { deleteDoneTasks } from "./scripts/components/DeleteDoneTasks.js";
 import { getLocalStorageTabParse } from "./scripts/components/LocalStorage.js";
 import { setActiveTab } from "./scripts/components/SetActiveTab.js";
 import { toggleAllTasksStatus } from "./scripts/components/ToggleAllTasksStatus.js";
-import { updateTaskList } from "./scripts/components/updateTaskList.js";
+import { updateTaskList } from "./scripts/components/UpdateTaskList.js";
 
 function App() {
   const activeTab = getLocalStorageTabParse();
