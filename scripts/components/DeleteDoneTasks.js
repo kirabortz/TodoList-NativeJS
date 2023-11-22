3578db0410d51c31a5b34f56a7b7dbe27800b53a
BladeDancer <69kir969@mail.ru>
2023-11-22 11:26:01 +0300

    renamed component

---
+++ scripts/components/DeleteDoneTasks.js
@@ -0,0 +1,16 @@
+import { updateTaskList } from "./updateTaskList.js";
+import { getLocalStorageParse, setLocalStorage, getLocalStorageTabParse } from "./LocalStorage.js";
+
+export const deleteDoneTasks = () => {
+  const clearCompletedBtn = document.querySelector(".clear_completed_btn");
+
+  clearCompletedBtn.addEventListener("click", () => {
+    const activeTab = getLocalStorageTabParse();
+    const tasks = getLocalStorageParse();
+
+    const inCompleteTasks = tasks.filter((task) => !task.completed);
+
+    setLocalStorage(inCompleteTasks);
+    updateTaskList(activeTab);
+  });
+};
